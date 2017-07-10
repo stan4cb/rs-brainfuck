ad3a80701c020a259bc9c1279a0ac3c59c12a293
Stanislav Ursache <d34dkn16h7@gmail.com>
2017-07-11 02:37:19 +0300

    all passing test and clean

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,4 @@
-fn main() {
-    println!(
-        "{:?}",
-        brain_luck(
-            ",>+>>>>++++++++++++++++++++++++++++++++++++++++++++>++++++++++++++++++++++++++++++++<<<<<<[>[>>>>>>+>+<<<<<<<-]>>>>>>>[<<<<<<<+>>>>>>>-]<[>++++++++++[-<-[>>+>+<<<-]>>>[<<<+>>>-]+<[>[-]<[-]]>[<<[>>>+<<<-]>>[-]]<<]>>>[>>+>+<<<-]>>>[<<<+>>>-]+<[>[-]<[-]]>[<<+>>[-]]<<<<<<<]>>>>>[++++++++++++++++++++++++++++++++++++++++++++++++.[-]]++++++++++<[->-<]>++++++++++++++++++++++++++++++++++++++++++++++++.[-]<<<<<<<<<<<<[>>>+>+<<<<-]>>>>[<<<<+>>>>-]<-[>>.>.<<<[-]]<<[>>+>+<<<-]>>>[<<<+>>>-]<<[<+>-]>[<+>-]<<<-]",
-            vec![2]
-        )
-    );
-}
+fn main() {}
 
 // advance_tests panics with nowhere to jump ?????
 // multiply_tests overflow
@@ -15,118 +7,97 @@ fn main() {
 fn brain_luck(code: &str, input: Vec<u8>) -> Vec<u8> {
     let mut i = 0;
     let mut stack_ptr = 0;
-    let mut stack_ptri : i32 = 0;
 
-    let mut jmp = false;
+    let mut jmp_depth = 0;
 
     let mut jumps = vec![];
     let mut stack: Vec<u8> = vec![0];
-    let mut nstack: Vec<u8> = vec![0];
     let mut output: Vec<u8> = vec![];
     let mut params = input.into_iter();
+    let chars = code.as_bytes();
 
     loop {
-        if let Some(op) = code.chars().nth(i) {
-
-            let t_stack = select_stack(&mut nstack, &mut stack, stack_ptri);
-
-            // jump until
-            if jmp {
-                // end jump
-                if op == ']' {
-                    jmp = false;
-                    println!("");
-                }
-            } else {
-                match op {
-                    '[' => {
-                        // jump wihtout executing between
-                        if t_stack[index(stack_ptri)] {
-                            jmp = true;
-                        } else {
-                            // set jump target
-                            jumps.push(i);
-                        }
-                    }
-                    ']' => {
-                        jmp = false;
-
-                        if 0 == 0 {
-                            // no need to jump back, remove last jump addr and continue
-                            jumps.pop();
-                        } else {
-                            // return to last jump
-                            if jumps.len() > 0 {
-                                i = jumps[jumps.len() - 1] - 1;
-                                jumps.pop();
-                            } else {
-                                panic!("Nowhere to jump");
-                            }
-                        }
-                    }
-                    '.' => {
-                        output.push(0);
-                    }
-                    ',' => {
-                        stack[stack_ptr] = params.next().expect("No param left");
-                    }
-                    '+' => {
-                        if stack[stack_ptr] != 255 {
-                            stack[stack_ptr] += 1;
-                        } else {
-                            stack[stack_ptr] = 0;
-                        }
-                    }
-                    '-' => {
-                        if stack[stack_ptr] != 0 {
-                            stack[stack_ptr] -= 1;
-                        } else {
-                            stack[stack_ptr] = 255;
-                        }
-                    }
-                    '>' => {
-                        stack_ptr += 1;
-
-                        // extend stack if needed
-                        if stack.len() <= stack_ptr {
-                            stack.push(0);
-                        }
-                    }
-                    '<' => {
-                        if stack_ptr > 0 {
-                            stack_ptr -= 1;
-                        } else {
-                            panic!("Negative stack not implemented");
-                        }
-                    }
-                    _ => {}
-                }
-            }
-
-
-            i += 1;
-        } else {
+        if i >= chars.len() {
             break;
         }
+
+        let op = chars[i] as char;
+
+        // jump until
+        if jmp_depth != 0 {
+            // end jump
+            match op {
+                '[' => jmp_depth += 1,
+                ']' => jmp_depth -= 1,
+                _ => {}
+            }
+        } else {
+            match op {
+                '[' => {
+                    // jump wihtout executing between
+                    if stack[stack_ptr] == 0 {
+                        jmp_depth += 1;
+                    } else {
+                        // set jump target
+                        jumps.push(i);
+                    }
+                }
+                ']' => {
+                    if stack[stack_ptr] == 0 {
+                        // no need to jump back, remove last jump addr and continue
+                        jumps.pop();
+                    } else {
+                        // return to last jump
+                        if jumps.len() > 0 {
+                            let pos = jumps.pop().unwrap();
+                            i = pos - 1;
+                        } else {
+                            panic!("Nowhere to jump");
+                        }
+                    }
+                }
+                '.' => {
+                    output.push(stack[stack_ptr]);
+                }
+                ',' => {
+                    stack[stack_ptr] = params.next().expect("No param left");
+                }
+                '+' => {
+                    if stack[stack_ptr] != 255 {
+                        stack[stack_ptr] += 1;
+                    } else {
+                        stack[stack_ptr] = 0;
+                    }
+                }
+                '-' => {
+                    if stack[stack_ptr] != 0 {
+                        stack[stack_ptr] -= 1;
+                    } else {
+                        stack[stack_ptr] = 255;
+                    }
+                }
+                '>' => {
+                    stack_ptr += 1;
+
+                    // extend stack if needed
+                    if stack.len() <= stack_ptr {
+                        stack.push(0);
+                    }
+                }
+                '<' => {
+                    if stack_ptr > 0 {
+                        stack_ptr -= 1;
+                    } else {
+                        panic!("Negative stack not implemented");
+                    }
+                }
+                _ => {}
+            }
+        }
+        i += 1;
     }
 
     output
-}
-
-fn index(i : i32) -> usize {
-    if i > 0 {
-        i as usize
-    } else {
-        (-i) as usize
-    }
-}
-
-fn select_stack<'a>(neg: &'a mut Vec<u8>,pos: &'a mut Vec<u8>, i: i32) -> &'a mut Vec<u8> {
-    if i > 0 {
-        pos
-    } else {
-        neg
-    }
 }
 
 #[test]
@@ -138,7 +109,7 @@ fn read_until_255() {
     assert_eq!(brain_luck(",+[-.,+]", vec![1, 1, 1, 255]), vec![1, 1, 1]);
 }
 #[test]
-fn a() {
+fn print_a() {
     assert_eq!(
         brain_luck("++++++ [ > ++++++++++ < - ] > +++++ .", vec![]),
         vec![65]
@@ -153,7 +124,7 @@ fn multiply() {
     assert_eq!(
         brain_luck(
             ",>,< [ > [ >+ >+ << -] >> [- << + >>] <<< -] >>.",
-            vec![8, 3]
+            vec![8, 3],
         ),
         vec![24]
     );
@@ -161,34 +132,33 @@ fn multiply() {
 #[test]
 fn hello_world() {
     assert_eq!(
-        brain_luck( "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.",
-            vec![]
+        brain_luck(
+            "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]
+            >>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.",
+            vec![],
         ),
         "Hello World!\n".as_bytes()
     );
 }
 
 #[test]
-fn hello_world_s() {
+fn division() {
     assert_eq!(
         brain_luck(
-"++++++++++
-[>+++++++>++++++++++>+++>+<<<<-] The initial loop to set up useful values in the array
->++.                             Print 'H'
->+.                              Print 'e'
-+++++++.                         Print 'l'
-.                                Print 'l'
-+++.                             Print 'o'
->++.                             Print ' '
-<<+++++++++++++++.               Print 'W'
->.                               Print 'o'
-+++.                             Print 'r'
-------.                          Print 'l'
---------.                        Print 'd'
->+.                              Print '!'
->.                               Print newline",
-            vec![]
+            "
+,>,>++++++[-<--------<-------->>]
+<<[
+>[->+>+<<]
+>[-<<-
+[>]>>>[<[>>>-<<<[-]]>>]<<]
+>>>+
+<<[-<<+>>]
+<<<]
+>[-]>>>>[-<<<<<+>>>>>]
+<<<<++++++[-<++++++++>]<.
+",
+            vec![2, 1],
         ),
-        "Hello World!\n".as_bytes()
+        vec![2]
     );
 }
